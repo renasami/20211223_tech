267565b15385013769f8e1f4357f1e93c37da790
renasami <drrr0502@gmail.com>
2021-12-22 23:50:50 +0900

    add todo

---
+++ svelte-todo/src/App.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import Task from "./Task.svelte"
+	import {taskList} from "./stores"
+	let text = ""
+	const addTask = () => {
+		if (!text) return
+		taskList.update((task) => [...task,{
+			id:task.length,
+			text:text,
+			done:false
+		}])
+		text = ""
+	}
+
+</script>
+
+<h1>svelte todo</h1>
+<hr />
+<div>
+	<input bind:value={text}/>
+	<button on:click={addTask}>追加</button>
+	<ul>
+	{#each $taskList as task }
+		{#if !task.done}
+			<Task task={task}/>
+		{/if}
+	{/each}
+	</ul>
+</div>
+
---
+++ svelte-todo/src/Task.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+    import {taskList} from "./stores"
+    import type {TaskType} from "./types"
+
+    export let task:TaskType
+    const handleOnClick = () => {
+        const newTask:TaskType[] = $taskList.map((t,i) => {
+            if (task.id !== i) return t
+            return {
+                id: t.id,
+                text: t.text,
+                done:!t.done
+            }
+        })
+        taskList.update(() => [...newTask])
+    }
+</script>
+
+<li>
+    <button on:click={handleOnClick}>削除</button>
+    <p>{task.text}</p>
+</li>
+
+<style>
+    li {
+        display:flex;
+        margin:10px;
+    }
+
+    p {
+        margin:0;
+        margin-left: 10px;
+    }
+</style>
